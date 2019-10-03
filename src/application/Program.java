fff643c21f27b22a4703122d17d9fe2c4c9188aa
megamega11 <marcelsbranco@hotmail.com>
2019-10-03 14:29:53 -0300

    Departamento entity class

---
+++ src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+import model.entities.Departamento;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+
+		Departamento dp = new Departamento(2214, "Guitarras");
+		
+		System.out.println(dp);
+
+	}
+
+}
